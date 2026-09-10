6cfb398c505f552cfb4bfe68827b3cece541643a
Misael-cruz <63094473+slockmr@users.noreply.github.com>
2026-09-10 14:22:05 -0600

    feat(documents): route private institutional sample downloads

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PrivateDeliveryDownloadController;
+use App\Http\Controllers\PrivateFormatSampleDownloadController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/app');
@@ -9,4 +10,6 @@ Route::redirect('/login', '/app/login')->name('login');
 Route::middleware('auth')->group(function (): void {
     Route::get('/app/deliveries/{delivery}/files/{file}', PrivateDeliveryDownloadController::class)
         ->name('planning-deliveries.download');
+    Route::get('/admin/format-samples/{sample}/files/{file}', PrivateFormatSampleDownloadController::class)
+        ->name('format-samples.download');
 });
