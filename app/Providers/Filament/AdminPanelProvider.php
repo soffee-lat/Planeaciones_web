c3ff6986038f0aaf79959b8185ae1cc4c478d330
Misael-cruz <63094473+slockmr@users.noreply.github.com>
2026-09-10 14:23:07 -0600

    feat(documents): register institutional formats admin resource

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -11,6 +11,7 @@ class AdminPanelProvider extends BasePanelProvider {
             ->pages([\App\Filament\Admin\Pages\Dashboard::class])
             ->resources([
                 \App\Filament\Resources\PlanningRequests\PlanningRequestResource::class,
+                \App\Filament\Resources\InstitutionalFormats\InstitutionalFormatResource::class,
                 \App\Filament\Resources\Curricula\CurriculumResource::class,
                 \App\Filament\Resources\CurriculumVersions\CurriculumVersionResource::class,
                 \App\Filament\Resources\Plans\PlanResource::class,
