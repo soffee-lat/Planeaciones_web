9615014509f8e4274fb415974081f4a49978c797
Misael-cruz <63094473+slockmr@users.noreply.github.com>
2026-09-11 13:24:33 -0600

    feat(validation): route curriculum map workflow

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CurriculumMapController;
 use App\Http\Controllers\InstitutionalFormatDesignerController;
 use App\Http\Controllers\InstitutionalFormatVisualBindingController;
 use App\Http\Controllers\InstitutionalFormatVisualPreviewController;
@@ -14,6 +15,17 @@ Route::redirect('/login', '/app/login')->name('login');
 Route::middleware('auth')->group(function (): void {
     Route::get('/app/deliveries/{delivery}/files/{file}', PrivateDeliveryDownloadController::class)->name('planning-deliveries.download');
     Route::get('/app/format-samples/{sample}/files/{file}', PrivateFormatSampleDownloadController::class)->name('format-samples.download');
+
+    Route::get('/app/planning-requests/{planningRequest}/curriculum-map', [CurriculumMapController::class, 'show'])
+        ->name('planning.curriculum-map');
+    Route::post('/app/planning-requests/{planningRequest}/curriculum-map/decision', [CurriculumMapController::class, 'decision'])
+        ->name('planning.curriculum-map.decision');
+    Route::post('/app/planning-requests/{planningRequest}/curriculum-map/accept-all', [CurriculumMapController::class, 'acceptAll'])
+        ->name('planning.curriculum-map.accept-all');
+    Route::post('/app/planning-requests/{planningRequest}/curriculum-map/add', [CurriculumMapController::class, 'add'])
+        ->name('planning.curriculum-map.add');
+    Route::post('/app/planning-requests/{planningRequest}/curriculum-map/confirm', [CurriculumMapController::class, 'confirm'])
+        ->name('planning.curriculum-map.confirm');
 
     Route::get('/app/institutional-formats/{format}/designer', InstitutionalFormatDesignerController::class)
         ->name('institutional-formats.designer');
