2ef27fd97fa6aa41ae639147829cc9ebb63e96f2
Misael-cruz <63094473+slockmr@users.noreply.github.com>
2026-09-18 08:24:14 -0600

    feat(ui): prioritize teacher planning navigation

--- app/Filament/App/Resources/PlanningRequests/PlanningRequestResource.php
+++ app/Filament/App/Resources/PlanningRequests/PlanningRequestResource.php
@@ -49,7 +49,7 @@ class PlanningRequestResource extends Resource
 
     protected static ?string $navigationLabel = 'Mis planeaciones';
 
-    protected static ?int $navigationSort = 30;
+    protected static ?int $navigationSort = 10;
 
     public static function getEloquentQuery(): Builder
     {
